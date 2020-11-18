47c90c75b0d783244aefc1947e26da32dd01a9ab
phong <phongnguyendx@gmail.com>
2020-11-18 12:01:17 +0700

    sang18

---
+++ admin/assets/js/add-job.js
@@ -0,0 +1,27 @@
+function setup() {
+    cbDisable = document.getElementById('idisable');
+    lbDi = document.getElementById('lbdi');
+    cbDisable.addEventListener('change', changeValueD);
+}
+
+function changeValueD() {
+    if (cbDisable.getAttribute('value') == 'not') {
+        cbDisable.setAttribute('value', 'checked');
+        lbdi.innerHTML = 'True';
+    } else {
+        cbDisable.setAttribute('value', 'not');
+        lbdi.innerHTML = 'False';
+    }
+    console.log(cbDisable.getAttribute('value'));
+}
+
+function addJob(){
+    var image = document.getElementById('image');
+    var name = document.getElementById('name').value;
+    var company = document.getElementById('company').value;
+    var salary = document.getElementById('salary').value;
+    var address = document.getElementById('address').value;
+    var status = document.getElementById('idisable').value;
+    //console.log(name+company+salary+address+status);
+   console.log(image.value); 
+}
